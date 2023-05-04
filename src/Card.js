cbb210f4613490dcf99a6d71071b9669bd6b5a34
Spawn9986 <scouch9986@gmail.com>
2023-05-03 20:41:06 -0500

    destructuring

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow">
-      <img alt="robots" src={`https://robohash.org/${props.id}?200x200`} />
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
 };
 
 export default Card;
+
+//Before Destructuring
+/* const Card = (props) => {
+    const { name, email, id } = props; */
