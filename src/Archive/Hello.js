f769e69841e11ab1266617348b4f16aa886cbde3
Spawn9986 <scouch9986@gmail.com>
2023-05-03 20:04:25 -0500

    added a component Card

--- src/Archive/Hello.js
+++ src/Archive/Hello.js
@@ -26,3 +26,18 @@ class Hello extends Component {
 } */
 
 export default Hello;
+
+//index.js
+
+//import React from 'react';
+//import ReactDOM from 'react-dom/client'; 
+//import './index.css'; // when importing from non JS file
+//import Hello from './Archive/Hello'; // when importing from file
+//import 'tachyons'; // when importing from dependencies
+/* import App from './App'; */
+//import reportWebVitals from './reportWebVitals';
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+ // <Hello greeting={'Hello React Ninja'}/>
+//);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import './index.css'; // when importing from non JS file
-import Hello from './Hello'; // when importing from file
+import Card from './Card'; // when importing from file
 import 'tachyons'; // when importing from dependencies
 /* import App from './App'; */
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Hello greeting={'Hello React Ninja'}/>
+  <Card />
 );
 
 
